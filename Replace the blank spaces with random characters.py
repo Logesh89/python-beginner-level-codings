85c9097766edb03f6d69b7a662f3a29bae679072
LOGESH.S <107561275+Logesh89@users.noreply.github.com>
2022-09-10 11:27:30 +0530

    Add files via upload
    Replace the blank spaces with random characters with python.

---
+++ Replace the blank spaces with random characters.py
@@ -0,0 +1,15 @@
+word_list=["ardvark","baboon","camel"]
+import random
+chosen_word=random.choice(word_list)
+print(f"Pssst , The solution is {chosen_word}.")
+display=[]
+word_length=len(chosen_word)
+for _ in range (word_length):
+    display+="_"
+    #print(display)
+guess=input("Guess a letter: ").lower()
+for position in range (word_length):
+    letter=chosen_word[position]
+    if letter==guess:
+        display[position]=letter
+        print(display)
