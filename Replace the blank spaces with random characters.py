e471f83b368cf0cd9ee9b112f9e73c5089a289d2
LOGESH.S <107561275+Logesh89@users.noreply.github.com>
2022-09-10 11:51:46 +0530

    Update Replace the blank spaces with random characters.py

--- Replace the blank spaces with random characters.py
+++ Replace the blank spaces with random characters.py
@@ -6,10 +6,10 @@ display=[]
 word_length=len(chosen_word)
 for _ in range (word_length):
     display+="_"
-    #print(display)
+print(display)
 guess=input("Guess a letter: ").lower()
 for position in range (word_length):
     letter=chosen_word[position]
     if letter==guess:
         display[position]=letter
-        print(display)
+print(display)
